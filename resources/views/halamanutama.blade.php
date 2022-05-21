f042ef351ec1053ec872c8065aa38cb1ca14545b
Tegar Adisaputra <tegara25@gmail.com>
2022-05-21 18:17:46 +0700

    tanpa searchbar

--- resources/views/halamanutama.blade.php
+++ resources/views/halamanutama.blade.php
@@ -2,12 +2,13 @@
 @section('container')
 @include('layout.header')
 
-<div class="searchbar mt-5">
+<!-- fitur search coming soon -->
+<!-- <div class="searchbar mt-5">
     <form>
         <input class="kolomsearch"type="text" placeholder="Search..." name="search">
         <i class=" btn fas fa-search searchbtn"></i>
     </form>
-</div>
+</div> -->
 <div id="demo" class="carousel slide mt-5" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
