9d402a7f52362b73f93966086170051d909c69b1
arbhi <arbhiuser@gmail.com>
2022-06-30 01:12:56 +0700

    halaman restoran di halman utama admin lebih baik fornt end nya

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -35,31 +35,5 @@
     </div>
     <div class="col-md-3"></div>
 </div>
-<div class="col-md-12 kotak mt-5">
-    <div class="row">
-        <h3 class="mt-2 ml-2 text-center">Request Pending</h3>
-    </div>
-    <div class="row kolom">
-        <table class="table table-striped text-center ">
-            <tr>
-                <th>No.</th>
-                <th>Tanggal</th>
-                <th>Nama Restoran</th>
-                <th>Alamat</th>
-                <th>Pilihan</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>2 Juli 2022</td>
-                <td>Angkringan</td>
-                <td>Ukraina</td>
-                <td>
-                    <button class="btn btn-success">Setujui</button>
-                    <button class="btn btn-primary">Detail</button>
-                    <button class="btn btn-danger ml-5">Tolak</button>
-                </td>
-            </tr>
-        </table>
-    </div>
-</div>
+
 @endsection()
